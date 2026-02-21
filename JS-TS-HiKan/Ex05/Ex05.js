2d02592edbe5275ed068ef463f792f90c9458df1
LuxKaito <khang.ng243@gmail.com>
2026-02-21 10:39:43 +0700

    [Ex05] - Function

---
+++ JS-TS-HiKan/Ex05/Ex05.js
@@ -0,0 +1,18 @@
+
+
+function greeting(){
+    console.log("Hello, welcome to JavaScript!");
+}
+greeting();
+
+// sum function
+// function sum(a, b){
+//     return a + b;
+// }
+
+// Arrow function
+const sum = (a, b) => {
+    return a + b;
+}
+console.log("Tổng của 5 và 3 là:" ,sum(5, 3));
+
