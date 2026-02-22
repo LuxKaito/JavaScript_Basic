df0ea279069aa93e4b174dd484ad4ad1b7e6535d
LuxKaito <khang.ng243@gmail.com>
2026-02-22 21:45:31 +0700

    [Ex06] - Array

---
+++ JS-TS-HiKan/Ex06/Ex06.js
@@ -0,0 +1,18 @@
+
+// Array of names
+const names = ["Alice", "Bob", "Charlie", "David", "Eve"];
+
+console.log("Original array:", names);
+
+const others = ["HiKan", 25, [1, 2, 3]] // lưu truu nhiều kiểu dữ liệu khác nhau trong một mảng
+
+console.log("Other array:", others);
+
+console.log("First name:", names[0]); // Truy cập phần tử đầu tiên của mảng
+console.log("Last name:", names[names.length - 1]); // Truy cập phần tử cuối cùng của mảng
+
+// CHinh sửa phần tử trong mảng
+names[3] = "Alicia";
+console.log("Modified array:", names);
+
+console.log("Third name:", names[2]);   
