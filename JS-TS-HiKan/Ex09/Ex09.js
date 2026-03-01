e7c0c395f4f9bed0f371bb4f458730e3b4b763eb
LuxKaito <khang.ng243@gmail.com>
2026-03-01 21:32:59 +0700

    [Ex09] - Object

---
+++ JS-TS-HiKan/Ex09/Ex09.js
@@ -0,0 +1,24 @@
+
+const scores = [10 , 4, 8, 6];
+
+//key: value
+const sv1 = {
+    score: 10,
+    name: "Khang",
+    address: {
+        city: "HCM",
+        district: "Binh Thanh"
+    }
+}
+const sv2 = {
+    score: 4,
+    name: "Khoa",
+    address: {
+        city: "HCM",
+        district: "Binh Thanh"
+    }   
+}
+
+const sinhvien = [sv1, sv2];
+console.log("Scores:", scores);
+console.log(sinhvien);
