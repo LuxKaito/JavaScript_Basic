74f284aba723c4fd01dc0ed486a2e6f6a2e0704d
LuxKaito <khang.ng243@gmail.com>
2026-03-02 17:31:55 +0700

    [Ex11] -LocalStorage  update

--- JS-TS-HiKan/Ex11/Ex11.js
+++ JS-TS-HiKan/Ex11/Ex11.js
@@ -2,6 +2,12 @@
 const btn = document.getElementById("submitBtn");
 const usernameInput = document.getElementById("nameInput");
 
+const outputNameStored = document.getElementById("outputNameStored");
+const outputName = localStorage.getItem("username");
+if (outputName) {
+  outputNameStored.innerHTML = `Welcome back, ${outputName}!`;
+}
+
 const storedUsername = localStorage.getItem("username");
 btn.addEventListener("click", () => {
   // console.log(usernameInput.value);
