ba571a926548a157a5e694af2f7a4e45dfa8e61b
LuxKaito <khang.ng243@gmail.com>
2026-08-03 08:57:00 +0700

    update syntax

--- NodeJS-Tutorial/02-Express-MVC/src/lib/prisma.ts
+++ NodeJS-Tutorial/02-Express-MVC/src/lib/prisma.ts
@@ -3,12 +3,6 @@ import "dotenv/config";
 import { PrismaClient } from "../generated/prisma/client.ts";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 
-const adapter = new PrismaMariaDb({
-    host: process.env.DB_HOST || "localhost",
-    port: Number(process.env.DB_PORT) || 3306,
-    user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "",
-    database: process.env.DB_NAME || "nodejs_db",
-});
+const adapter = new PrismaMariaDb(process.env.DATABASE_URL || "mysql://root:password@localhost:3306/mydb");
 
 export const prisma = new PrismaClient({ adapter });
